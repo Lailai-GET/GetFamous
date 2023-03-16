b472779d6d68e36c40a992f34c2b46f121c56c2c
Nikolaipe <nikolaipe@getacademy.no>
2023-03-16 14:40:41 +0100

    Initial commit

---
+++ Sheeple.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetFamous
+{
+    public class Sheeple
+    {
+        public string SheepName { get; private set; }
+        public bool Fan { get; private set; }
+        public string FandomStatus { get; private set; }
+        public int ExploitableFunds { get; private set; }
+
+        public Sheeple(string sheepName, bool fan, string fandomStatus, int exploitableFunds)
+        {
+            SheepName = sheepName;
+            Fan = fan;
+            FandomStatus = fandomStatus;
+            ExploitableFunds = exploitableFunds;
+        }
+        public static object ReadFans()
+        {
+            //https://stackoverflow.com/questions/4462921/how-to-create-a-property-for-a-listt
+        }
+    }
+}
---
+++ Star.cs
@@ -0,0 +1,16 @@
+﻿namespace GetFamous
+{
+    public class Star
+    {
+        public string StarName { get; private set; }
+        public List<Sheeple> Sheeples { get; private set;}
+        public int Funds { get; private set; }
+
+        public Star(string starName, List<Sheeple> sheeples, int funds)
+        {
+            StarName = starName;
+            Sheeples = sheeples;
+            this.Funds = funds;
+        }
+    }
+}
